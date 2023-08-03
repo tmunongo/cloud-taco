675dab69780dedfa9bcbf0daa4ff8be8e21f88b6
Tawanda Munongo <ejmunongo@gmail.com>
2023-08-03 17:42:08 +0200

    inital commit with many things already

---
+++ src/main/java/webbe/tacocloud/Controllers/HomeController.java
@@ -0,0 +1,12 @@
+package webbe.tacocloud.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+}
